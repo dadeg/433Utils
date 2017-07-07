159a802761a8d95124b451074a85b9b2500da080
Dan DeGreef <daniel_degreef@condenast.com>
2017-07-07 15:04:17 -0500

    add 315 receiver

---
+++ RPi_utils/315-RFSnifferMQTT.cpp
@@ -0,0 +1,58 @@
+/*
+  RFSnifferMQTT
+
+  Usage: ./RFSnifferMQTT
+*/
+
+#include "../rc-switch/RCSwitch.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+     
+     
+RCSwitch mySwitch;
+ 
+
+
+int main(int argc, char *argv[]) {
+  
+     // This pin is not the first pin on the RPi GPIO header!
+     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+     // for more information.
+     int PIN = 26;
+     
+     if(wiringPiSetup() == -1) {
+       printf("wiringPiSetup failed, exiting...");
+       return 0;
+     }
+
+     mySwitch = RCSwitch();
+     mySwitch.enableReceive(PIN);  // Receiver on interrupt 0 => that is pin #2
+     
+    
+     while(1) {
+  
+      if (mySwitch.available()) {
+    
+        int value = mySwitch.getReceivedValue();
+        
+	    std::ostringstream oss;
+		oss << "mosquitto_pub -t 315mhz/" << value << " -m CLOSED";
+		std::string mqttCommand = oss.str();
+	    
+	    system(mqttCommand.c_str());
+        printf("Received value: %i\n", value);
+    
+        mySwitch.resetAvailable();
+    
+      }
+      
+  
+  }
+
+  exit(0);
+
+
+}
+
